534850e5b5cd0cc6a449487c9e8ff47a3ab21c07
Roy Palomino Rojas <roy100995@gmail.com>
2024-02-17 12:11:47 -0500

    build: add prettier support for astro files

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: ['prettier-plugin-astro'],
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+}
+
+export default config
