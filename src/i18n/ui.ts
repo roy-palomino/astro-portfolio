f34c135958cbe4b4799ef0475a950f99ed53492f
Roy Palomino Rojas <roy100995@gmail.com>
2024-02-27 11:42:47 -0500

    feat: add new common items for i18n

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,23 +1,41 @@
 export const languages = {
-  en: 'English',
-  es: 'Español',
+  en: "English",
+  es: "Español",
 };
 
-export const defaultLang = 'en';
+export const defaultLang = "en";
 
 export const ui = {
   en: {
-    'nav.home': 'Home',
-    'nav.skills': 'Skills',
-    'nav.projects': 'Projects',
-    'nav.experience': 'Experience',
-    'nav.contact': 'Contact',
+    "nav.home": "Home",
+    "nav.skills": "Skills",
+    "nav.projects": "Projects",
+    "nav.experience": "Experience",
+    "nav.contact": "Contact",
+    "common.greeting": "Hi, my name is Roy",
+    "common.iAm": "I am a Full Stack Web developer",
+    "common.socials": "Socials",
+    "common.talk": "Let's talk",
+    "common.work": "My work",
+    "common.mySkills": "My skills",
+    "common.programmingLanguages": "Programming languages",
+    "common.tools": "Tools",
+    "common.responsive": "Responsive design",
   },
   es: {
-    'nav.home': 'Inicio',
-    'nav.skills': 'Habilidades',
-    'nav.projects': 'Proyectos',
-    'nav.experience': 'Experiencia',
-    'nav.contact': 'Contacto',
+    "nav.home": "Inicio",
+    "nav.skills": "Habilidades",
+    "nav.projects": "Proyectos",
+    "nav.experience": "Experiencia",
+    "nav.contact": "Contacto",
+    "common.greeting": "Hola, me llamo Roy",
+    "common.iAm": "Soy Desarrollador Web Full Stack",
+    "common.socials": "Redes sociales",
+    "common.talk": "Hablemos",
+    "common.work": "Mi trabajo",
+    "common.mySkills": "Mis habilidades",
+    "common.programmingLanguages": "Lenguajes de programación",
+    "common.tools": "Herramientas",
+    "common.responsive": "Diseño responsivo",
   },
 } as const;
