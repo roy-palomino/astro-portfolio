67bd5e9dcdac07ab5d5992c21b0ae46d1bd4554d
Roy Palomino Rojas <roy100995@gmail.com>
2024-05-04 17:35:14 -0500

    feat: add i18n ui text for buttons and body

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -12,6 +12,18 @@ export const ui = {
     "nav.work": "Work",
     "nav.skills": "Skills",
     "nav.contact": "Contact",
+    "hero.hello": "Hello, I am",
+    "hero.webDev": "Web Developer",
+    "hero.hi": "Hi, I'm",
+    "hero.iAm":
+      "I am a web developer with over 6 years of experience building dynamic and scalable web applications.",
+    "hero.passionate":
+      "I am passionate about my work and enjoy facing new challenges, learning from them, and continuously improving my skills.",
+    "hero.curious":
+      "I have a great curiosity for new technologies and I am always looking for ways to innovate and improve my projects.",
+    "button.downloadCV": "Download CV",
+    "miscellaneous.goToTop": "Go to top",
+    "miscellaneous.copyright": "Almost all rights reserved.",
   },
   es: {
     "nav.home": "Inicio",
@@ -19,5 +31,17 @@ export const ui = {
     "nav.work": "Trabajo",
     "nav.skills": "Habilidades",
     "nav.contact": "Contacto",
+    "hero.hello": "Hola, me llamo",
+    "hero.webDev": "Desarrollador Web",
+    "hero.hi": "Hola, me llamo",
+    "hero.iAm":
+      "Soy un desarrollador web con más de 6 años de experiencia construyendo aplicaciones web dinámicas y escalables.",
+    "hero.passionate":
+      "Soy apasionado por mi trabajo y disfruto enfrentando nuevos desafíos, aprendiendo de ellos y mejorando continuamente mis habilidades.",
+    "hero.curious":
+      "Tengo una gran curiosidad por las nuevas tecnologías y siempre busco maneras de innovar y mejorar mis proyectos.",
+    "button.downloadCV": "Descargar CV",
+    "miscellaneous.goToTop": "Volver al inicio",
+    "miscellaneous.copyright": "Casi todos los derechos reservados.",
   },
 } as const;
