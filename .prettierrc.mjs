62f04ec8fa07612dd7da67a2e7b682e322c71f8b
Roy Palomino Rojas <roy100995@gmail.com>
2024-05-04 17:34:57 -0500

    refactor: change prettier config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,10 +1,16 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
+  singleQuote: false,
+  semi: true,
   plugins: ["prettier-plugin-astro"],
   overrides: [
     {
       files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+      onSave: [],
     },
   ],
 };
