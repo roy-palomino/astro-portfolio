e1c8bdfe5170a5bbb26d5ed3fff0b06372349ce1
Roy Palomino Rojas <roy100995@gmail.com>
2024-05-12 15:26:31 -0500

    feat: add section description and miscelaneous changes

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -22,26 +22,43 @@ export const ui = {
     "hero.curious":
       "I have a great curiosity for new technologies and I am always looking for ways to innovate and improve my projects.",
     "button.downloadCV": "Download CV",
-    "miscellaneous.goToTop": "Go to top",
-    "miscellaneous.copyright": "Almost all rights reserved.",
-    "section.experience": "A journey through my web development career, where I've gained valuable skills and knowledge in diverse challenging projects.",
-    "section.work": "Concrete examples of my web development work, showcasing my skills in building dynamic and scalable web applications.",
-    "section.skills": "The technical and soft skills that allow me to tackle complex web projects and deliver high-quality solutions.",
-    "section.contact": "If you have any questions or would like to work with me, feel free to contact me. I will be happy to help you.",
+    "section.experience":
+      "A journey through my web development career, where I've gained valuable skills and knowledge in diverse challenging projects.",
+    "section.work":
+      "Concrete examples of my web development work, showcasing my skills in building dynamic and scalable web applications.",
+    "section.skills":
+      "The technical and soft skills that allow me to tackle complex web projects and deliver high-quality solutions.",
+    "section.contact":
+      "If you have any questions or would like to work with me, feel free to contact me. I will be happy to help you.",
     "experience.first.1": "Figma interface design.",
-    "experience.first.2": "Frontend development with React, Next, Typescript and Tailwind.",
+    "experience.first.2":
+      "Frontend development with React, Next, Typescript and Tailwind.",
     "experience.first.3": "Rest API Communication with Axios.",
     "experience.first.4": "Unit testing with Jest.",
     "experience.second.1": "Figma interface design.",
-    "experience.second.2": "Frontend development with Vue, Nuxt, Typescript and Element plus.",
+    "experience.second.2":
+      "Frontend development with Vue, Nuxt, Typescript and Element plus.",
     "experience.second.3": "Rest API communication with Axios.",
     "experience.second.4": "Manual testing.",
-    "experience.second.5": "Support and maintenance of legacy systems with Django REST, Vue, Bootstrap and JQuery.",
+    "experience.second.5":
+      "Support and maintenance of legacy systems with Django REST, Vue, Bootstrap and JQuery.",
     "experience.third.1": "Interface design",
-    "experience.third.2": "Frontend development with Solid, React and Bootstrap.",
+    "experience.third.2":
+      "Frontend development with Solid, React and Bootstrap.",
     "experience.third.3": "Backend development with Python and Django.",
     "role.frontend": "Frontend Developer",
     "role.fullstack": "Frontend and Backend Developer",
+    "andap.description":
+      "Professional training endorsed. It offers courses, diplomas and training for the development of work competencies, certified by leading companies.",
+    "escuelaMujer.description":
+      "This non-profit organization provides certified training courses and workshops, promoting personal and professional growth for attendees. A space to develop women's skills.",
+    "quilla.description":
+      "Offers a solid theoretical and practical training to future extrajudicial conciliators. Its team of qualified professionals imparts the teachings with proactivity, optimism, and commitment, guaranteeing quality training for students.",
+    "gal.description":
+      "This group aimed to create a space dedicated to ensuring access to justice for all citizens. They provide effective defense, informing clients about the real possibilities of each case. In addition to legal advisory services, they offer specialized training programs.",
+    "miscellaneous.goToTop": "Go to top",
+    "miscellaneous.copyright": "Almost all rights reserved.",
+    "miscellaneous.showMore": "Show more",
   },
   es: {
     "nav.home": "Inicio",
@@ -59,25 +76,41 @@ export const ui = {
     "hero.curious":
       "Tengo una gran curiosidad por las nuevas tecnologías y siempre busco maneras de innovar y mejorar mis proyectos.",
     "button.downloadCV": "Descargar CV",
-    "miscellaneous.goToTop": "Volver al inicio",
-    "miscellaneous.copyright": "Casi todos los derechos reservados.",
-    "section.experience": "Un recorrido por mi carrera en desarrollo web, donde he adquirido valiosas habilidades y conocimientos en diversos proyectos desafiantes.",
-    "section.work": "Ejemplos concretos de mi trabajo en desarrollo web, mostrando mis habilidades en la construcción de aplicaciones web dinámicas y escalables.",
-    "section.skills": "Las habilidades técnicas y blandas que me permiten abordar proyectos web complejos y entregar soluciones de alta calidad.",
-    "section.contact": "Si tienes alguna pregunta o te gustaría trabajar conmigo, no dudes en contactarme. Estaré encantado de ayudarte.",
+    "section.experience":
+      "Un recorrido por mi carrera en desarrollo web, donde he adquirido valiosas habilidades y conocimientos en diversos proyectos desafiantes.",
+    "section.work":
+      "Ejemplos concretos de mi trabajo en desarrollo web, mostrando mis habilidades en la construcción de aplicaciones web dinámicas y escalables.",
+    "section.skills":
+      "Las habilidades técnicas y blandas que me permiten abordar proyectos web complejos y entregar soluciones de alta calidad.",
+    "section.contact":
+      "Si tienes alguna pregunta o te gustaría trabajar conmigo, no dudes en contactarme. Estaré encantado de ayudarte.",
     "experience.first.1": "Diseño de interfaces en Figma.",
-    "experience.first.2": "Desarrollo frontend con React, Next, Typescript y Tailwind.",
+    "experience.first.2":
+      "Desarrollo frontend con React, Next, Typescript y Tailwind.",
     "experience.first.3": "Comunicación con API REST con Axios.",
     "experience.first.4": "Pruebas unitarias con Jest.",
     "experience.second.1": "Diseño de interfaces en Figma.",
-    "experience.second.2": "Desarrollo frontend con Vue, Nuxt, Typescript y Element plus.",
+    "experience.second.2":
+      "Desarrollo frontend con Vue, Nuxt, Typescript y Element plus.",
     "experience.second.3": "Comunicación con API REST con Axios.",
     "experience.second.4": "Pruebas manuales.",
-    "experience.second.5": "Soporte y mantenimiento de sistemas legados con Django REST, Vue, Bootstrap y JQuery.",
+    "experience.second.5":
+      "Soporte y mantenimiento de sistemas legados con Django REST, Vue, Bootstrap y JQuery.",
     "experience.third.1": "Diseño de interfaces",
     "experience.third.2": "Desarrollo frontend con Solid, React y Bootstrap.",
     "experience.third.3": "Desarrollo backend con Python y Django.",
     "role.frontend": "Desarrollador Frontend",
     "role.fullstack": "Desarrollador Frontend y Backend",
+    "andap.description":
+      "Centro de formación profesional avalada. Imparte cursos, diplomados y capacitaciones para el desarrollo de competencias laborales, certificadas por empresas líderes.",
+    "escuelaMujer.description":
+      "ONG que brinda capacitaciones y talleres con certificación, fomentando el desarrollo personal y profesional de las asistentes. Un espacio de aprendizaje y crecimiento enfocado en potenciar las habilidades femeninas.",
+    "quilla.description":
+      "Ofrece una sólida formación teórica y práctica a futuros conciliadores extrajudiciales. Su equipo de profesionales calificados imparte las enseñanzas con proactividad, optimismo y compromiso, garantizando una capacitación de calidad para los estudiantes.",
+    "gal.description":
+      "Este grupo se propuso crear un espacio dedicado a garantizar el acceso a la justicia para todos los ciudadanos.Realizan una defensa efectiva, informando a los usuarios sobre las posibilidades reales de cada caso. Además de la asesoría legal, brindan capacitaciones especializadas.",
+    "miscellaneous.goToTop": "Volver al inicio",
+    "miscellaneous.copyright": "Casi todos los derechos reservados.",
+    "miscellaneous.showMore": "Mostrar más",
   },
 } as const;
