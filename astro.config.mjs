c9893a54ed33ed26dd252e5d3a4ec45081d2355d
Roy Palomino Rojas <roy100995@gmail.com>
2024-05-20 18:10:13 -0500

    add auto config for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), react()],
   output: "static",
   site: 'https://roy-palomino.github.io',
+  adapter: netlify()
 });
